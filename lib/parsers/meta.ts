36ef701554066bd0b659c06035b82366282f8b05
scottlynn1 <scottlynn234@yahoo.com>
2025-07-30 12:46:47 -0400

    correct login for grabbing convo block

--- lib/parsers/meta.ts
+++ lib/parsers/meta.ts
@@ -10,13 +10,13 @@ export async function parseMeta(html: string): Promise<Conversation> {
   const styles = $('head style').toString();
   const links = $('head link').toString();
   const head = `<head>${styles}${links}</head>`;
-  const input = $('div[role="textbox"]');
-  console.log('input', input.length, input.toString());
-  const topDiv = input.parents('div').last();
-  console.log('topDiv', topDiv.length, topDiv.toString());
-  const convoDiv = topDiv.prev();
-  console.log('convoDiv', convoDiv.length, convoDiv.toString());
-  const convo = convoDiv.toString();
+  let input = $('div[role="textbox"]');
+  let sibling = null;
+  while (!sibling) {
+    input = input.parent('div')
+    sibling = input.prev();
+  }
+  const convo = sibling.toString();
   console.log('convo', convo.length, convo);
   // const divs = $('div.x78zum5.xdt5ytf.x1na6gtj.xz9dl7a.xsag5q8.xh8yej3').toString();
   const conversation = '<html class="_9dls _asb0 __fb-light-mode" lang="en" dir="ltr">' + head + convo + '</html>';
