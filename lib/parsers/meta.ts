06f7d36e8fef574cae90f87dbc151c6ad37eabe9
scottlynn1 <scottlynn234@yahoo.com>
2025-07-30 11:51:24 -0400

    change logic to select for the test input and then the sibling div above it

--- lib/parsers/meta.ts
+++ lib/parsers/meta.ts
@@ -10,8 +10,12 @@ export async function parseMeta(html: string): Promise<Conversation> {
   const styles = $('head style').toString();
   const links = $('head link').toString();
   const head = `<head>${styles}${links}</head>`;
-  const divs = $('div.x78zum5.xdt5ytf.x1na6gtj.xz9dl7a.xsag5q8.xh8yej3').toString();
-  const conversation = '<html class="_9dls _asb0 __fb-light-mode" lang="en" dir="ltr">' + head + divs + '</html>';
+  const input = $('[role="main"]')
+  const topDiv = input.parents('div').last();
+  const convoDiv = topDiv.prev();
+    // `.last()` gives the outermost div
+  // const divs = $('div.x78zum5.xdt5ytf.x1na6gtj.xz9dl7a.xsag5q8.xh8yej3').toString();
+  const conversation = '<html class="_9dls _asb0 __fb-light-mode" lang="en" dir="ltr">' + head + convoDiv + '</html>';
   return {
     model: 'Meta',
     content: conversation,
